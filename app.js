6eee1a791412d4af3331d284458be49306d95534
PragnyaKalvakol <pragnyareddy6789@gmail.com>
2022-04-06 19:10:47 -0500

    abcd

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var yogurtRouter = require('./routes/yogurt');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -24,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/yogurt', yogurtRouter);
-app.use('/addmods', addmodsRouter);
+app.use('/addmods', addmodsRouter);\
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
